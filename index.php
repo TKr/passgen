c3944129a3069640839d3122c24b1b3bf29a9ee6
Paweł Pyrczak <pawel.pyrczak@kissdigital.pl>
2016-11-09 11:48:44 +0100

    refactor

--- index.php
+++ index.php
@@ -5,13 +5,6 @@ require_once __DIR__.'/bootstrap.php';
 
 $service = new PassService;
 
-$pass1 = $service->generateSecurePasswordString(6);
-$pass2 = $service->generateSecurePasswordString(8);
-$pass3 = $service->generateSecurePasswordString(10);
-$pass4 = $service->generateSecurePasswordString(12);
-$pass5 = $service->generateSecurePasswordString(14);
-$pass6 = $service->generateSecurePasswordString(16);
-
 ?><!DOCTYPE html>
 <html>
     <head>
@@ -22,11 +15,11 @@ $pass6 = $service->generateSecurePasswordString(16);
         </style>
     </head>
     <body>
-        <span>6: <input onClick="this.select();" type="text" value="<?php echo $pass1?>"/></span><br/>
-        <span>8: <input onClick="this.select();" type="text" value="<?php echo $pass2?>"/></span><br/>
-        <span>10:<input onClick="this.select();" type="text" value="<?php echo $pass3?>"/></span><br/>
-        <span>12:<input onClick="this.select();" type="text" value="<?php echo $pass4?>"/></span><br/>
-        <span>14:<input onClick="this.select();" type="text" value="<?php echo $pass5?>"/></span><br/>
-        <span>16:<input onClick="this.select();" type="text" value="<?php echo $pass6?>"/></span><br/>
+        <span>6: <input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(6); ?>"/></span><br/>
+        <span>8: <input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(8); ?>"/></span><br/>
+        <span>10:<input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(10); ?>"/></span><br/>
+        <span>12:<input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(12); ?>"/></span><br/>
+        <span>14:<input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(14); ?>"/></span><br/>
+        <span>16:<input onClick="this.select();" type="text" value="<?php echo $service->generateSecurePasswordString(16); ?>"/></span><br/>
     </body>
 </html>
